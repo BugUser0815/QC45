4bf6f9dfd39cdb58aea76fc4636ba3f70bb0462f
Alexander Rothner <reisner.alex93@gmail.com>
2026-08-29 13:11:03 +0200

    Test standard OCPP 1.5 MeterValues request wrapper

--- native-integration/src/test/java/de/rothner/qc45/Ocpp15BridgeServerTest.java
+++ native-integration/src/test/java/de/rothner/qc45/Ocpp15BridgeServerTest.java
@@ -8,14 +8,14 @@ public final class Ocpp15BridgeServerTest {
     @Test
     public void forwardsEveryMeterGroupAndSample() throws Exception {
         String xml = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" "
-            + "xmlns:cs=\"urn://Ocpp/Cs/2012/06/\"><soap:Body><cs:meterValues>"
+            + "xmlns:cs=\"urn://Ocpp/Cs/2012/06/\"><soap:Body><cs:meterValuesRequest>"
             + "<cs:connectorId>2</cs:connectorId><cs:transactionId>77</cs:transactionId>"
             + "<cs:values><cs:timestamp>2026-08-27T08:00:00Z</cs:timestamp>"
             + "<cs:values><cs:value>1234</cs:value><cs:measurand>Energy.Active.Import.Register</cs:measurand><cs:unit>Wh</cs:unit></cs:values>"
             + "<cs:values><cs:value>16.2</cs:value><cs:measurand>Current.Import</cs:measurand><cs:phase>L1</cs:phase><cs:unit>A</cs:unit></cs:values>"
             + "</cs:values><cs:values><cs:timestamp>2026-08-27T08:00:10Z</cs:timestamp>"
             + "<cs:values><cs:value>10.5</cs:value><cs:measurand>Power.Active.Import</cs:measurand><cs:unit>kW</cs:unit></cs:values>"
-            + "</cs:values></cs:meterValues></soap:Body></soap:Envelope>";
+            + "</cs:values></cs:meterValuesRequest></soap:Body></soap:Envelope>";
 
         assertEquals(
             "{\"connectorId\":2,\"transactionId\":77,\"meterValue\":["
@@ -27,12 +27,24 @@ public final class Ocpp15BridgeServerTest {
             Ocpp15BridgeServer.meterValuesJson(xml));
     }
 
+    @Test
+    public void keepsLegacyMeterValuesWrapperCompatible() throws Exception {
+        String xml = "<meterValues><connectorId>1</connectorId><values>"
+            + "<timestamp>2026-08-27T08:00:00Z</timestamp><values>"
+            + "<value>42</value><unit>Wh</unit></values></values></meterValues>";
+
+        assertEquals(
+            "{\"connectorId\":1,\"meterValue\":[{\"timestamp\":\"2026-08-27T08:00:00Z\","
+            + "\"sampledValue\":[{\"value\":\"42\",\"unit\":\"Wh\"}]}]}",
+            Ocpp15BridgeServer.meterValuesJson(xml));
+    }
+
     @Test(expected = Exception.class)
     public void externalEntitiesAreRejected() throws Exception {
         Ocpp15BridgeServer.meterValuesJson(
             "<!DOCTYPE x [<!ENTITY e SYSTEM \"file:///etc/passwd\">]>"
-            + "<meterValues><connectorId>1</connectorId><values>"
+            + "<meterValuesRequest><connectorId>1</connectorId><values>"
             + "<timestamp>2026-08-27T08:00:00Z</timestamp><values>"
-            + "<value>&e;</value></values></values></meterValues>");
+            + "<value>&e;</value></values></values></meterValuesRequest>");
     }
 }
