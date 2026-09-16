06ac30d0da90fcf0c33b60f260727e573986e20b
Alexander Rothner <reisner.alex93@gmail.com>
2026-09-16 11:35:48 +0200

    Overlay QC45 safety recovery details on charging screen

--- ui-patch/src/main/java/pt/efacec/es/evcsd/ui/AlpitronicChargingPanel.java
+++ ui-patch/src/main/java/pt/efacec/es/evcsd/ui/AlpitronicChargingPanel.java
@@ -4,6 +4,8 @@ import pt.efacec.es.evcsd.ui.info.ChargeInfo;
 
 /** Active sessions all use the approved Modbus-backed charging monitor. */
 class AlpitronicChargingPanel extends WaitingForCardChargingTimer {
+    private final SafetyStatusOverlay safetyStatus;
+
     AlpitronicChargingPanel(int modus, boolean usesCreditCard) {
         this(modus, usesCreditCard, 0);
     }
@@ -11,6 +13,21 @@ class AlpitronicChargingPanel extends WaitingForCardChargingTimer {
     AlpitronicChargingPanel(int modus, boolean usesCreditCard, int connector) {
         super("de", modus, usesCreditCard, true, connector);
         AlpitronicSessionState.markCharging();
+        safetyStatus = new SafetyStatusOverlay();
+        safetyStatus.setBounds(0, 416, 640, 64);
+        add(safetyStatus);
+        setComponentZOrder(safetyStatus, 0);
+        safetyStatus.startMonitoring();
+    }
+
+    public void start() {
+        super.start();
+        safetyStatus.startMonitoring();
+    }
+
+    public void stop() {
+        safetyStatus.stopMonitoring();
+        super.stop();
     }
 
     public void setInfo(ChargeInfo info) {
